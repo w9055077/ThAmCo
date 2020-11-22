1b5f084c9ef98d3cd54f6aae79a5d0bc2c794f7e
JOE DINSLEY <W9055077@live.tees.ac.uk>
2020-11-22 16:16:11 +0000

    Added FoodItemsController

--- ThAmCo.Catering.Api/Startup.cs
+++ ThAmCo.Catering.Api/Startup.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using ThAmCo.Catering.Models;
 
-namespace ThAmCo.Catering
+namespace ThAmCo.Catering.Api
 {
     public class Startup
     {
@@ -25,8 +25,7 @@ namespace ThAmCo.Catering
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<CateringContext>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +35,13 @@ namespace ThAmCo.Catering
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
