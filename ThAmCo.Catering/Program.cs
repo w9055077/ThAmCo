700270be24f1d1810f83838d2c6fe54d1ac69dff
JOE DINSLEY <W9055077@live.tees.ac.uk>
2020-11-22 16:37:26 +0000

    Made the database delete everytime it runs for development purposes

--- ThAmCo.Catering/Program.cs
+++ ThAmCo.Catering/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ThAmCo.Catering.Models;
 
 namespace ThAmCo.Catering
 {
@@ -14,7 +17,21 @@ namespace ThAmCo.Catering
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var env = services.GetRequiredService<IHostingEnvironment>();
+                if (env.IsDevelopment())
+                {
+                    var context = services.GetRequiredService<CateringContext>();
+                    context.Database.EnsureDeleted();
+                    context.Database.Migrate();
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
