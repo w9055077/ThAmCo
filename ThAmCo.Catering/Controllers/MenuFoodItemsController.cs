7dea4377742c373ee9229562c0343d3649b593e8
JOE DINSLEY <W9055077@live.tees.ac.uk>
2020-12-08 13:44:51 +0000

    Moved MenuFoodItems to correct folder.

--- ThAmCo.Catering/Controllers/MenuFoodItemsController.cs
+++ ThAmCo.Catering/Controllers/MenuFoodItemsController.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ThAmCo.Catering.Models;
 
-namespace ThAmCo.Catering.Models
+namespace ThAmCo.Catering.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
