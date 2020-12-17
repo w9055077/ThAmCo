dd5d4b1e66cfcfb910907ad29cb3bb2923af19af
JOE DINSLEY <W9055077@live.tees.ac.uk>
2020-12-17 16:16:42 +0000

    Added Customer MVC

--- ThAmCo.Catering/Models/FoodBooking.cs
+++ ThAmCo.Catering/Models/FoodBooking.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ThAmCo.Events.Data;
 
 namespace ThAmCo.Catering.Models
 {
